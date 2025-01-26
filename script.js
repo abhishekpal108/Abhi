c0841d0bbe07f8ac31b2667db184e69a91af2ef3
Abhishek Pal <41889690+abhishekpal108@users.noreply.github.com>
2025-01-26 14:50:24 +0100

    Update script.js
    removed the const content section

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function toggleTheme() {
     const theme = body.classList.contains('day') ? 'night' : 'day';
     body.className = theme;
 
-    const content = theme === 'day'
-        ? `<h1>Welcome to My Portfolio</h1>
-           <p>I am Abhi, a Database Administrator with 10 years of experience.</p>
-           <p>Based in Brussels, Belgium.</p>`
-        : `<h1>Welcome to My Portfolio</h1>
-           <p>At night, I enjoy playing badminton, tennis, and Dota2.</p>
-           <p>Also, I love listening to music while traveling or working.</p>`;
-    document.getElementById('content').innerHTML = content;
+//   const content = theme === 'day'
+//       ? `<h1>Welcome to My Portfolio</h1>
+//          <p>I am Abhi, a Database Administrator with 10 years of experience.</p>
+//          <p>Based in Brussels, Belgium.</p>`
+//       : `<h1>Welcome to My Portfolio</h1>
+//          <p>At night, I enjoy playing badminton, tennis, and Dota2.</p>
+//          <p>Also, I love listening to music while traveling or working.</p>`;
+//   document.getElementById('content').innerHTML = content;
 	
 	
 	
